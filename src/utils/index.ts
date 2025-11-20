49448e533c80a59094313910621dfc7b13c6d2a0
FarazMehraien <farazmehraien95@gmail.com>
2025-11-20 22:33:27 +0330

    add plate mask

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,29 @@ import { twMerge } from 'tailwind-merge'
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
+
+export const plateMaskHander = (raw: string) => {
+  const mask = ['D', 'D', 'A', 'D', 'D', 'D', '-', 'D', 'D']
+  let value = ''
+  let i = 0
+
+  for (const m of mask) {
+    if (raw[i] === undefined) break
+    const char = raw[i]
+
+    if (m === 'D') {
+      if (/[0-9]/.test(char)) value += char
+      else break
+    } else if (m === 'A') {
+      if (/[\u0627-\u06CC]/.test(char)) value += char
+      else break
+    } else if (m === '-') {
+      value += '-'
+      i--
+    }
+
+    i++
+  }
+
+  return value
+}
